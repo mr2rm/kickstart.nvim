62246886185501b379acc0562259e410247801ff
MRez <mrez.rahmani@gmail.com>
2024-04-21 15:49:43 +0200

    chore(utils): open Lazygit on submodules using Telescope

--- lua/custom/plugins/utils/lazygit.lua
+++ lua/custom/plugins/utils/lazygit.lua
@@ -16,11 +16,27 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = '[L]azy[G]it' },
+      {
+        '<leader>slg',
+        function()
+          require('telescope').extensions.lazygit.lazygit()
+        end,
+        desc = '[S]earch [L]azy[G]it Repository',
+      },
     },
-    -- TODO: Set keymap to open Lazygit via Telescope
     config = function()
       require('telescope').load_extension 'lazygit'
+
+      -- That makes sure that any opened buffer which is contained in a git
+      -- repo will be tracked.
+      vim.api.nvim_create_autocmd('BufEnter', {
+        desc = 'Load repository path',
+        pattern = '*',
+        callback = function()
+          require('lazygit.utils').project_root_dir()
+        end,
+      })
     end,
   },
 }
