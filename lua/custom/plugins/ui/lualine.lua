0273581392eb972943405a0150e5e5f885e23664
MRez <mrez.rahmani@braincreators.com>
2024-04-26 23:21:23 +0200

    feat(ui): install lua-line

--- lua/custom/plugins/ui/lualine.lua
+++ lua/custom/plugins/ui/lualine.lua
@@ -1,3 +1,32 @@
+--- @param hide_width number? hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ''
+    end
+    -- runcate
+    if trunc_len and #str > trunc_len then
+      if trunc_width == nil or win_width < trunc_width then
+        return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+      end
+    end
+    -- No change
+    return str
+  end
+end
+
+local function get_active_venv()
+  local venv = require('venv-selector').get_active_venv()
+  if venv then
+    local venv_parts = vim.fn.split(venv, '/')
+    local venv_name = venv_parts[#venv_parts]
+    return venv_name
+  end
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
@@ -18,76 +47,27 @@ return {
     opts = function()
       vim.o.laststatus = vim.g.lualine_laststatus
 
-      --- @param trunc_width number? trunctates component when screen width is less then trunc_width
-      --- @param trunc_len number truncates component to trunc_len number of chars
-      --- @param hide_width number? hides component when window width is smaller then hide_width
-      --- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
-      --- return function that can format the component accordingly
-      local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
-        return function(str)
-          local win_width = vim.fn.winwidth(0)
-          -- Hide
-          if hide_width and win_width < hide_width then
-            return ''
-          end
-          -- Truncate
-          if trunc_len and #str > trunc_len then
-            if trunc_width == nil or win_width < trunc_width then
-              return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
-            end
-          end
-          -- No change
-          return str
-        end
-      end
-
       return {
         options = { theme = 'tokyonight' },
         extensions = { 'nvim-tree', 'lazy' },
+        -- TODO: Manage sections on lesswidth
         sections = {
           lualine_a = { 'mode' },
           lualine_b = {
             { 'branch', fmt = trunc(nil, 25, nil, false) },
           },
           lualine_c = {
-            {
-              'diagnostics',
-              symbols = {
-                error = ' ',
-                warn = ' ',
-                info = ' ',
-                hint = ' ',
-              },
-            },
+            -- {
+            --   'diagnostics',
+            --   symbols = {
+            --     error = ' ',
+            --     warn = ' ',
+            --     info = ' ',
+            --     hint = ' ',
+            --   },
+            -- },
             { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
-            { 'filename' },
-          },
-          lualine_x = {
-            -- stylua: ignore
-            {
-              function()
-                return require('noice').api.status.command.get()
-              end,
-              cond = function()
-                return package.loaded['noice'] and require('noice').api.status.command.has()
-              end,
-              color = { fg = '#ff9e64' },
-            },
-            -- stylua: ignore
-            {
-            function()
-              return require('noice').api.status.mode.get()
-            end,
-            cond = function()
-              return package.loaded['noice'] and require('noice').api.status.mode.has()
-            end,
-            color = { fg = '#ff9e64' },
-            },
-            -- stylua: ignore
-            {
-              require('lazy.status').updates,
-              cond = require('lazy.status').has_updates,
-            },
+            { 'filename', path = 4 },
             {
               'diff',
               symbols = {
@@ -107,14 +87,47 @@ return {
               end,
             },
           },
+          lualine_x = {
+            {
+              function()
+                return require('noice').api.status.command.get()
+              end,
+              cond = function()
+                return package.loaded['noice'] and require('noice').api.status.command.has()
+              end,
+              color = { fg = '#ff9e64' },
+            },
+            {
+              function()
+                return require('noice').api.status.mode.get()
+              end,
+              cond = function()
+                return package.loaded['noice'] and require('noice').api.status.mode.has()
+              end,
+              color = { fg = '#ff9e64' },
+            },
+            {
+              require('lazy.status').updates,
+              cond = require('lazy.status').has_updates,
+            },
+          },
+          -- NOTE: This section needs improvements
+          -- TODO: Use Trouble instead of lsp-status
+          -- TODO: Venv selector has extension for lualine but didn't work
           lualine_y = {
-            { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
-            { 'location', padding = { left = 0, right = 1 } },
+            "require('lsp-status').status()",
+            {
+              get_active_venv,
+              icon = '',
+              color = { fg = '#CDD6F4' },
+              cond = function()
+                return get_active_venv() ~= nil
+              end,
+            },
           },
           lualine_z = {
-            function()
-              return ' ' .. os.date '%R'
-            end,
+            { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
+            { 'location', padding = { left = 0, right = 1 } },
           },
         },
         tabline = {
