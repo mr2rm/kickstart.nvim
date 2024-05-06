3c978622a975a60cc0f0ddce5fa2741d425347ed
MRez <mrez.rahmani@gmail.com>
2024-05-07 01:44:38 +0200

    chore(utils): improve Neogit configuration

--- lua/custom/plugins/utils/neogit.lua
+++ lua/custom/plugins/utils/neogit.lua
@@ -4,11 +4,26 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim', -- required
       'sindrets/diffview.nvim', -- optional - Diff integration
+      -- TODO: Configure diffview
 
       -- Only one of these is needed, not both.
       'nvim-telescope/telescope.nvim', -- optional
       -- 'ibhagwan/fzf-lua', -- optional
     },
     config = true,
+    opts = {
+      graph_style = 'unicode',
+      signs = {
+        section = { '', '' },
+        item = { '', '' },
+      },
+      integrations = {
+        telescope = true,
+        diffview = true,
+      },
+    },
+    keys = {
+      { '<leader>gn', '<cmd>Neogit<CR>', desc = '[N]eogit' },
+    },
   },
 }
