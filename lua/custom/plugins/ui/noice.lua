ad13b587ebce8010c84f7c1cf056b3080a6674ad
MRez <mrez.rahmani@braincreators.com>
2024-05-09 20:03:42 +0200

    perf(ui): back to default throttle for LSP progress in Noice

--- lua/custom/plugins/ui/noice.lua
+++ lua/custom/plugins/ui/noice.lua
@@ -10,9 +10,6 @@ return {
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
-        progress = {
-          throttle = 1000 / 3,
-        },
       },
       -- you can enable a preset for easier configuration
       presets = {
