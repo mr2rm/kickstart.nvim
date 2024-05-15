33731f8551e3e4c6a83e8d79f9abc9c055e00cfe
MRez <mrez.rahmani@braincreators.com>
2024-05-15 11:14:12 +0200

    chore(utils): clean Venv Selector configuration

--- lua/custom/plugins/utils/venv-selector.lua
+++ lua/custom/plugins/utils/venv-selector.lua
@@ -1,9 +1,3 @@
--- TODO: Activate venv by moving to a directory
--- If .envrc exists, only need to copy shell $PATH to current $PATH
--- Otherwise we need to check if there is a valid venv, then:
---    1. Active the venv
---    2. Update current $PATH
-
 return {
   {
     'linux-cultist/venv-selector.nvim',
@@ -27,28 +21,5 @@ return {
       -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
       { '<leader>vc', '<cmd>VenvSelectCached<cr>', desc = '[C]ached' },
     },
-    -- config = function()
-    --   vim.api.nvim_create_autocmd('VimEnter', {
-    --     desc = 'Auto select virtualenv Nvim open',
-    --     pattern = '*',
-    --     callback = function()
-    --       local venv = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
-    --       if venv ~= '' then
-    --         require('venv-selector').retrieve_from_cache()
-    --       end
-    --     end,
-    --     once = true,
-    --   })
-    --   vim.api.nvim_create_autocmd('DirChanged', {
-    --     desc = 'Change venv if needed',
-    --     pattern = '*',
-    --     callback = function()
-    --       print(vim.fn.getcwd())
-    --       -- vim.fn.system { 'direnv', 'reload' }
-    --       -- vim.fn.system { 'poetry', 'shell' }
-    --       vim.fn.system { 'poetry', '-C', vim.fn.getcwd(), 'shell' }
-    --     end,
-    --   })
-    -- end,
   },
 }
