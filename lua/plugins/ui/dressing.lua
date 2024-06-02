93a3fa418f5b00a6ab5661ba57929ebffd931edf
MRez <mrez.rahmani@gmail.com>
2024-06-02 16:11:43 +0200

    chore(ui): lazy-load dressing

--- lua/plugins/ui/dressing.lua
+++ lua/plugins/ui/dressing.lua
@@ -1,6 +1,22 @@
 return {
   {
     'stevearc/dressing.nvim',
+    lazy = true,
+    init = function()
+      -- Lazy-load plugin on select
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.select(...)
+      end
+
+      -- Lazy-load plugin on input
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require('lazy').load { plugins = { 'dressing.nvim' } }
+        return vim.ui.input(...)
+      end
+    end,
     opts = {
       input = {
         insert_only = false,
