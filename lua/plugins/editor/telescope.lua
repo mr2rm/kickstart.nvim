27a1c3cf7843c2968694d063934ff6c3222de781
MRez <mrez.rahmani@braincreators.com>
2024-07-02 16:47:04 +0200

    chore(editor): find directories in Telescope and focous in Nvim-tree

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -133,6 +133,30 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Find directory and focus in Nvim-tree
+      -- NOTE: Needs fd to be installed (https://github.com/sharkdp/fd)
+      local function find_directory_and_focus()
+        local action_state = require 'telescope.actions.state'
+
+        local function open_nvim_tree(prompt_bufnr, _)
+          actions.select_default:replace(function()
+            local api = require 'nvim-tree.api'
+
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            api.tree.open()
+            api.tree.find_file(selection.cwd .. '/' .. selection.value)
+          end)
+          return true
+        end
+
+        require('telescope.builtin').find_files {
+          find_command = { 'fd', '--type', 'directory', '--exclude', '.git/*' },
+          attach_mappings = open_nvim_tree,
+        }
+      end
+      vim.keymap.set('n', '<leader>sD', find_directory_and_focus)
     end,
   },
 }
